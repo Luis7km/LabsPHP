ef6371670b49defdc94b13d29ce2f19974d56d5b
Luis7km <anthony.milla1997@gmail.com>
2022-10-27 21:30:47 -0500

    Laboratorio 17

---
+++ api/configuracion/conexion.php
@@ -0,0 +1,20 @@
+<?php
+    class Conexion {
+        //especificar las credenciales de base de datos
+        private $host="localhost";
+        private $db_name="productos_api";
+        private $username="root";
+        private $password="";
+        public $conn;
+        //obtenerlaconexiondelabasededatos
+        public function obtenerConexion(){
+            $this->conn=null;
+            try{
+                $this->conn=new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+                $this->conn->exec("set names utf8");
+            }catch(PDOException $exception){
+                echo"Error de conexion a base de datos:".$exception->getMessage();
+            }return $this->conn;
+        }
+    }
+?>
